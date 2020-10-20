ca0c583b3b80bc4fdd505ebd3a08df59c32047c3
jordan.bottrell@coretek.com <jordan.bottrell@coretek.com>
2020-10-19 21:17:41 -0400

    test.html

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     #Print("Is this thing on?")
     return "<h1>Hello World!</h1>"
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 if __name__ == '__main__':
     app.run()
