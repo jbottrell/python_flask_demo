b94c281e6d646e47e5f132c484b33cb7879f2cf4
jordan.bottrell@coretek.com <jordan.bottrell@coretek.com>
2020-10-19 20:51:09 -0400

    added debugging print statement

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/home/')
+@app.route('/')
 def homepage():
+    Print("Is this thing on?")
     return "Hello World!"
 
 if __name__ == '__main__':
