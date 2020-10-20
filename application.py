45610689df12b8cacfdd25673d9d8fe1650a2802
jordan.bottrell@coretek.com <jordan.bottrell@coretek.com>
2020-10-19 21:58:18 -0400

    I think I broke it lol

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('')
+@app.route('/')
 def homepage():
     #Print("Is this thing on?")
     return "<h1>Hello World!</h1>"
